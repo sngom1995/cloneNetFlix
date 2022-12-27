8924602c03ca5dc77df29cab840cdf9b6ad769cd
sngom1995 <ngomsamba75@gmail.com>
2022-12-27 06:50:33 +0000

    Initial Commit

---
+++ CloneNetFlix/CloneNetFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneNetFlixApp.swift
+//  CloneNetFlix
+//
+//  Created by Samba NGOM on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CloneNetFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
